ffbea84555e47b237311d2dc01fc09595d901d5a
weirdwizardtom <koristo1@fit.cvut.cz>
2020-12-30 17:52:12 +0100

    Removed the layout file for the sign in activity

--- app/src/main/java/com/weirdwizardtom/withoutme/SignInActivity.java
+++ app/src/main/java/com/weirdwizardtom/withoutme/SignInActivity.java
@@ -24,7 +24,6 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firebase_ui);
     }
 
     @Override
